81b9227b89b6e27aafe35a193a7bc09a39fce176
arvin.he <vk.he@qq.com>
2018-04-28 23:04:09 +0800

    Update test.sh
    添加输出：命令行参数用 $n 表示，例如，$1 表示第一个参数，$2 表示第二个参数，依次类推。

--- test.sh
+++ test.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+echo "File Name: $0"
+echo "First Parameter : $1"
+echo "First Parameter : $2"
+echo "Quoted Values: $@"
+echo "Quoted Values: $*"
+echo "Total Number of Parameters : $#"
+
+
+# print work dir
+pwd
+
 tmp = 'markriggins/todowrangler'
 
 repository=${repository:-$tmp}
